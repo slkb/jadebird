a1707f3dd40a1612f6a0a6d49f1a164c64661ae2
mengfanding <love@mengfanding.cn>
2016-03-19 12:06:38 +0800

    添加项目

---
+++ Aptech/src/u1/c1/Test.java
@@ -0,0 +1,30 @@
+package u1.c1;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		
+		List list = new ArrayList<>();
+		List list1 = new LinkedList<>();
+
+		Set set = new HashSet<>();
+		Set set1 = new TreeSet<>();
+
+		Map map = new HashMap<>();
+		Map map1 = new TreeMap<>();
+
+		Collection c=list;
+	}
+}
